3812d16b8eef02016003a4d617710a7e74df5ee5
Julio Valsesia <jcvalsesia@avaya.com>
2023-08-15 16:12:56 -0300

    Update comment

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ async fn main() {
     // build our application with a single route
     let app = Router::new().route("/bet/:num", get(new_bet));
 
-    // run it with hyper on localhost:3000
+    // run it with hyper on localhost:8080
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
